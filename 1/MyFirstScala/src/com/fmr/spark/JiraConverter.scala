e9324162db4e6510a9c1ca3ff4b45098674c9e54
Shan, Daniel <Yuxiao.Shan@fmr.com>
2016-11-30 21:56:47 +0800

    what can we do now

--- 1/MyFirstScala/src/com/fmr/spark/JiraConverter.scala
+++ 1/MyFirstScala/src/com/fmr/spark/JiraConverter.scala
@@ -3,11 +3,11 @@ import org.apache.spark.{ SparkConf, SparkContext }
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.DeserializationFeature
+import java.sql.DriverManager
 import play.api.libs.json._
-
 case class Person(name: String, lovesPandas: Boolean) // Note: must be a top level class
-case class Issue(issueId:String, project:String, team:String){
-  override def toString:String = "Issue [issueId=" + issueId + ", project=" + project + ", team=" + team + "]"
+case class Issue(key: String, project: String, team: String) {
+  override def toString: String = "Issue [issueId=" + key + ", project=" + project + ", team=" + team + "]"
 }
 object JiraConverter {
   def main(args: Array[String]) {
@@ -19,40 +19,47 @@ object JiraConverter {
     conf.setMaster("local[2]")
 
     val sc = new SparkContext(conf)
-    val input = sc.textFile("C:\\TestFiles\\pandainfo.json", 2)
-
+    //val input = sc.textFile("C:\\TestFiles\\pandainfo.json", 2)
+    val input = sc.textFile("C:\\TestFiles\\HDFS-JIRAQA-71.txt", 2)
+//val json: JsValue = Json.parse("C:\\TestFiles\\pandainfo.json")
+//val minifiedString: String = Json.stringify(json)
     val result = input.mapPartitions(records => {
-        // mapper object created on each executor node
-        val mapper = new ObjectMapper
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        mapper.registerModule(DefaultScalaModule)
-        // We use flatMap to handle errors
-        // by returning an empty list (None) if we encounter an issue and a
-        // list with one element if everything is ok (Some(_)).
-        records.flatMap(record => {
-          try {
-            Some(mapper.readValue(record, classOf[Person]))
-          } catch {
-            case e: Exception => None
-          }
-        })
+      // mapper object created on each executor node
+      val mapper = new ObjectMapper
+      mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+      mapper.registerModule(DefaultScalaModule)
+      // We use flatMap to handle errors
+      // by returning an empty list (None) if we encounter an issue and a
+      // list with one element if everything is ok (Some(_)).
+      records.flatMap(record => {
+        try {
+          Some(mapper.readValue(record, classOf[Issue]))
+        } catch {
+          case e: Exception => None
+        }
+      })
     }, true)
-       result.filter(_.lovesPandas).mapPartitions(records => {
+    //result.filter(_.key).mapPartitions(records => {
+    result.mapPartitions(records => {
       val mapper = new ObjectMapper
       mapper.registerModule(DefaultScalaModule)
       records.map(mapper.writeValueAsString(_))
     })
-      .saveAsTextFile("C:\\TestFiles\\output")
-      
-      
-      
-        val parsed = input.map(Json.parse)
-  implicit val personReads = Json.format[Issue]
- 
-val result1 = parsed.flatMap(record => personReads.reads(record).asOpt)
-result1.collect
+      .saveAsTextFile("C:\\TestFiles\\output2")
+
+    result.foreachPartition { records =>
+      records.foreach(record =>
+        {
+          Class.forName("oracle.jdbc.driver.OracleDriver").newInstance()
+          val lda = DriverManager.getConnection("jdbc:oracle:thin:@10.168.104.92:1521:Orcl", "ryan", "ryan123")
+          val sth = lda.createStatement()
+          sth.execute("insert into sys.jira_issues (issue_id) values('"
+            + record.key + "')")
+          sth.close()
+          lda.close()
+        })
     }
 
+  }
 
-  
 }
