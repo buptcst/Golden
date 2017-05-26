62382564fa6b1be78892ea646e9c585551700cea
Shan, Daniel <Yuxiao.Shan@fmr.com>
2017-05-26 05:25:09 -0400

    wtmsb

--- akka-rabbitmq-master/src/main/scala/com/thenewmotion/akka/rabbitmq/Message.scala
+++
@@ -1,9 +0,0 @@
-package com.thenewmotion.akka.rabbitmq
-
-import akka.actor.{ Props, ActorRef }
-
-case class CreateChannel(props: Props, name: Option[String] = None)
-
-case class ChannelCreated(channel: ActorRef)
-
-case class ChannelMessage(onChannel: OnChannel, dropIfNoChannel: Boolean = true)
--- hiworld/src/main/scala/Movie.scala
+++
@@ -1,3 +0,0 @@
-class Movie {
-  def watchMovie(): Boolean = true
-}
